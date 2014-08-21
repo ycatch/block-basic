cdbea798223caa347432772d8925c77e9ab478ac
ycatch <y-catch@ja2.so-net.ne.jp>
2014-08-22 01:06:19 +0900

    solved: ploblem of check Strinng or Number

--- js/bbb_script.js
+++ js/bbb_script.js
@@ -92,7 +92,7 @@ case 4:this.$ = "";
 break;
 case 5:this.$ = "alert(" + $$[$0] + ");";
 break;
-case 6:this.$ = $$[$0-3] + "=" + "prompt(" + $$[$0] + ");";
+case 6:this.$ = "var bbb_a = prompt(" + $$[$0] + "); " + $$[$0-3] + " = this.checkNumber(bbb_a);";
 break;
 case 7:this.$ = $$[$0-2] + "=" + $$[$0] + ";";
 break;
